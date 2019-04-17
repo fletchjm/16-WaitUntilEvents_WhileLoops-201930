cde594b822e01a1537db72638018390810719f13
fletchjm <fletchjm@rose-hulman.edu>
2019-04-17 11:39:03 -0400

    DONE

--- src/m4_wait_for_events.py
+++ src/m4_wait_for_events.py
@@ -342,7 +342,7 @@ def prime_gap(m):
       :type m: int
     """
     # -------------------------------------------------------------------------
-    # DONE: 5. Implement and test this function.
+    # TODO: 5. Implement and test this function.
     #   The testing code is already written for you (above).
     #
     # IMPLEMENTATION REQUIREMENT:
